fdae14bdbc6ddd65a87b8c8e94bd095349c3a52e
Leonid Ugadiarov <ugadiarov.la@phystech.edu>
2024-10-30 18:34:18 +0300

    Fixed bug when the best loss variable is not initialized

--- train_dlp.py
+++ train_dlp.py
@@ -1,6 +1,8 @@
 """
 Single-GPU training of DLPv2
 """
+import math
+
 # imports
 import numpy as np
 import os
@@ -140,14 +142,14 @@ def train_dlp(config_path='./configs/shapes.json'):
     scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=scheduler_gamma, verbose=True)
 
     # initialize validation statistics
-    valid_loss = best_valid_loss = None
+    valid_loss = best_valid_loss = math.inf
     valid_losses = []
     best_valid_epoch = -1
 
     # image metrics
     val_lpipss = []
     best_val_lpips_epoch = -1
-    val_lpips = best_val_lpips = None
+    val_lpips = best_val_lpips = math.inf
 
     pretrained_epoch = 0
     if load_model and pretrained_path is not None:
@@ -392,7 +394,7 @@ def train_dlp(config_path='./configs/shapes.json'):
             print(log_str)
             log_line(log_dir, log_str)
             do_save_best_weights = False
-            if best_valid_loss is None or best_valid_loss > valid_loss:
+            if math.isinf(best_valid_loss) is None or best_valid_loss > valid_loss:
                 log_str = f'validation loss updated: {best_valid_loss:.3f} -> {valid_loss:.3f}\n'
                 print(log_str)
                 log_line(log_dir, log_str)
@@ -417,7 +419,7 @@ def train_dlp(config_path='./configs/shapes.json'):
                 val_lpips = valid_imm_results['lpips']
                 print(log_str)
                 log_line(log_dir, log_str)
-                if (not torch.isinf(torch.tensor(val_lpips))) and (best_val_lpips is None or best_val_lpips > val_lpips):
+                if (not torch.isinf(torch.tensor(val_lpips))) and (math.isinf(best_val_lpips) or best_val_lpips > val_lpips):
                     log_str = f'validation lpips updated: {best_val_lpips:.3f} -> {val_lpips:.3f}\n'
                     print(log_str)
                     log_line(log_dir, log_str)
